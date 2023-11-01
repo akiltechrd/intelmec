c0c4507b53652292a5b806d11ea789867f61f509
frank <mbueno@akiltech.com>
2023-11-01 16:02:59 -0400

    reportes parametros

---
+++ frontend/views/site/contacto.php
@@ -0,0 +1,111 @@
+<!--page-header-->
+<div class="page-header has-image">
+    <div class="page-header-content">
+        <div class="featured-image"></div>
+        <div class="container">
+            <h1>Contact</h1>
+            <nav class="breadcrumbs">
+                <a class="home" href="#"><span>Home</span></a>
+                <i class="fa fa-angle-right" aria-hidden="true"></i>
+                <span>Contact</span>
+            </nav>
+        </div>
+    </div>
+</div>
+<!--page-header  end-->
+
+<!-- team-->
+<div class="contactpage pagepadd">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-4 col-md-12">
+                <div class="mf-contact-box clearfix ">
+                    <div class="mf-section-title text-left dark medium-size ">
+                        <h2>Headquater</h2></div>
+                    <div class="contact-info address"><i class="flaticon-arrow"></i>
+                        <div><span>Address:</span> 321 Car World, 2nd Breaking Str, Newyork ,USA 10002.</div>
+                    </div>
+                    <div class="contact-info phone"><i class="fa fa-phone"></i>
+                        <div><span>Call Us:</span> +321 4567 89 012 &amp; 79 023 021 40</div>
+                    </div>
+                    <div class="contact-info email"><i class="flaticon-note"></i>
+                        <div><span>Mail Us:</span> Supportteam@ManuFactory.com</div>
+                    </div>
+                    <ul class="contact-social">
+                        <li class="facebook">
+                            <a href="#" target="_blank">
+                                <i class="fa fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li class="twitter">
+                            <a href="#" target="_blank">
+                                <i class="fa fa-twitter"></i>
+                            </a>
+                        </li>
+                        <li class="googleplus">
+                            <a href="#" target="_blank">
+                                <i class="fa fa-google-plus"></i>
+                            </a>
+                        </li>
+                        <li class="skype">
+                            <a href="#" target="_blank">
+                                <i class="fa fa-skype"></i>
+                            </a>
+                        </li>
+                        <li class="linkedin">
+                            <a href="#" target="_blank">
+                                <i class="fa fa-linkedin"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-lg-8 col-md-12">
+                <div class="mf-department">
+                    <div class="mf-section-title text-left dark medium-size">
+                        <h2>Sales Department</h2>
+                    </div>
+                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3769.191949269061!2d-70.2528174!3d19.143073299999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8eafd21ad4ca7341%3A0xbc2e826ab3c25bc5!2sIntelmec!5e0!3m2!1sen!2sdo!4v1698868872084!5m2!1sen!2sdo" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</div>
+<div class="contactform">
+    <div class="container">
+        <div class="mf-section-title text-center dark large-size margbtm20">
+            <h2>Send Message Us</h2>
+        </div>
+        <div class="text-center margbtm40">Don’t hestiate to ask us something, Our customer support team always ready to help
+            <br>you,they will support you 24/7.</div>
+        <div class="contact-form mf-contact-form-7 form-light">
+            <form method="post" action="ajax/mail.php" class="wpcf7-form" id="contact-form" novalidate="novalidate">
+                <div class="mf-form mf-form-2">
+                    <div class="row">
+                        <div class="col-md-6 col-xs-12 col-sm-12 mf-input-field">
+                            <p>
+                                <input type="text" name="name" value="" size="40" placeholder="Your Name*">
+                            </p>
+                            <p>
+                                <input type="email" name="email" value="" size="40" placeholder="Email Address*">
+                            </p>
+                            <p>
+                                <input type="text" name="phone" value="" size="40" placeholder="Phone Number">
+                            </p>
+                        </div>
+                        <div class="col-md-6 col-xs-12 col-sm-12 mf-textarea-field">
+                            <p>
+                                <textarea name="message" cols="40" rows="10" placeholder="Enter Your Message..."></textarea>
+                            </p>
+                        </div>
+                        <div class="text-center mf-submit col-md-12 col-xs-12 col-sm-12">
+                            <button type="submit" class="btn-style-two">SEND</button>
+                        </div>
+                    </div>
+                </div>
+                <div id="loading" class="wpcf7-response-output wpcf7-display-none"><img src="images/ajax-loader.png" alt="loading" /></div>
+            </form>
+        </div>
+    </div>
+</div>
